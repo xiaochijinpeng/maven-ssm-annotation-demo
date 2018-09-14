08adaf3ee8dcf80904c3d5298e21f59e5d52912f
cutiechi <chi@cutie.ooo>
2018-09-14 16:55:57 +0800

    feat(config): add sql session factory bean config

--- src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
+++ src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
@@ -1,11 +1,16 @@
 package com.cutiechi.demo.config;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 /**
  * Spring Application Context 配置类
@@ -23,4 +28,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 )
 public class ApplicationContextConfig {
 
+    /**
+     * SQL Session Factory Bean 配置
+     *
+     * @param dataSource 数据源 Bean
+     * {@link DataSourceConfig 数据源配置类}
+     * @return SQL Session Factory Bean
+     */
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory (DataSource dataSource) {
+
+        // 定义 SQL Session Factory Bean
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+
+        // 为 SQL Session Factory Bean 设置数据源
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
 }
