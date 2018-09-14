6d7436a34b92b509f2940e4c4cc6ae63af111392
cutiechi <chi@cutie.ooo>
2018-09-14 18:43:35 +0800

    feat(config): add spring mvc component scan base package config

--- src/main/java/com/cutiechi/demo/config/DispatcherServletConfig.java
+++ src/main/java/com/cutiechi/demo/config/DispatcherServletConfig.java
@@ -1,5 +1,6 @@
 package com.cutiechi.demo.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.cutiechi.demo.web.controller")
 public class DispatcherServletConfig {
 
 }
