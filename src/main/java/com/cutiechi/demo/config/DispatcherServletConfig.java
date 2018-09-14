85f8f021c003a012040ff869ff04340e6e367098
cutiechi <chi@cutie.ooo>
2018-09-14 18:49:25 +0800

    feat(config): add spring mvc view resolver config

--- src/main/java/com/cutiechi/demo/config/DispatcherServletConfig.java
+++ src/main/java/com/cutiechi/demo/config/DispatcherServletConfig.java
@@ -1,9 +1,12 @@
 package com.cutiechi.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Spring MVC Dispatcher Servlet 配置类
@@ -15,4 +18,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan("com.cutiechi.demo.web.controller")
 public class DispatcherServletConfig {
 
+    /**
+     * Spring MVC 视图解析器 Bean
+     *
+     * @return 视图解析器 Bean
+     */
+    @Bean
+    public ViewResolver viewResolver () {
+
+        // 定义 InternalResourceResolver
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        // 设置视图解析器前缀为 /WEB-INF/pages/
+        viewResolver.setPrefix("/WEB-INF/pages/");
+
+        // 设置视图解析器后缀为 .jsp
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
