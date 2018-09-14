dbf6330adb9e499ca54ab48127c303f804ad258f
cutiechi <chi@cutie.ooo>
2018-09-14 18:30:02 +0800

    feat(dao): add list all user method

--- src/main/java/com/cutiechi/demo/dao/UserDao.java
+++ src/main/java/com/cutiechi/demo/dao/UserDao.java
@@ -1,6 +1,12 @@
 package com.cutiechi.demo.dao;
 
+import com.cutiechi.demo.model.entity.User;
+
+import org.apache.ibatis.annotations.Select;
+
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 用户数据访问对象接口
@@ -10,4 +16,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDao {
 
+    /**
+     * 获取全部用户列表
+     *
+     * @return 全部用户列表
+     */
+    @Select("select user_id, user_name, user_password from user")
+    List<User> listAll ();
 }
