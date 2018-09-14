d78b470f0ae880eda341244e3f739bec211bf389
cutiechi <chi@cutie.ooo>
2018-09-14 18:23:20 +0800

    feat(dao): add user dao interface

---
+++ src/main/java/com/cutiechi/demo/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 用户数据访问对象接口
+ *
+ * @author Cutie Chi
+ */
+@Repository
+public interface UserDao {
+
+}
